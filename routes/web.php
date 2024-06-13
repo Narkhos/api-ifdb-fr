bb79f8297c481c0d7f9ed07156b9113e61afcbe2
Narkhos <xavierdirez@hotmail.com>
2024-06-13 20:05:54 +0000

    routes info

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,20 @@
 use App\Http\Controllers\IfdbController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get(
+//     '/xdebug-info',
+//     [IfdbController::class, function () {
+//         return xdebug_info();
+//     }]
+// );
+
+// Route::get(
+//     '/php-info',
+//     [IfdbController::class, function () {
+//         return phpinfo();
+//     }]
+// );
+
 Route::get(
     '/',
     [IfdbController::class, 'index']
